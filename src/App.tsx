6fbc0e32211ba4dfc543b712d442b68030b82d35
chenhaojie9527 <a18520145493@163.com>
2023-06-18 20:22:58 +0800

    feat: 问卷列表

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
+  // 列表页
+  const questionList = [
+    {
+      id: 1,
+      title: "问卷1",
+      isPublished: false,
+    },
+    {
+      id: 2,
+      title: "问卷2",
+      isPublished: false,
+    },
+    {
+      id: 3,
+      title: "问卷3",
+      isPublished: true,
+    },
+    {
+      id: 4,
+      title: "问卷4",
+      isPublished: false,
+    },
+  ];
+
+  const onEdit = (id: number) => {
+    console.log("edit id: ", id);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto">
+      <h1 className="text-center mb-2">问卷列表页</h1>
+      <div className="flex items-center flex-col space-y-4">
+        {questionList.map((item) => {
+          const { id, title, isPublished } = item;
+          return (
+            <div
+              className="flex items-center space-x-2 border-b-2"
+              key={id}
+            >
+              <span>{title}</span>
+              {isPublished ? (
+                <span className="text-green-500">已发布</span>
+              ) : (
+                <span className="text-red-500">未发布</span>
+              )}
+              <button onClick={() => onEdit(id)}>编辑问卷</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
