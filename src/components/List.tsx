92350821002d068e117e7b9426cfd7f17bc5160d
chenhaojie9527 <a18520145493@163.com>
2023-06-20 17:27:45 +0800

    feat: useEffect使用

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,21 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import QuestionItem from "../common/QuestionItem";
 import { useImmer } from "../hooks";
 
 export const List: FC = () => {
+  /**
+   * 组件初次渲染时执行了两次useEffect
+   * 实际上是先渲染了一次，然后销毁了一次，然后有渲染了一次
+   * 这是从React18开始，useEffect在开发环境下会执行两次
+   * 原因是模拟组件创建，销毁，再创建的完整流程，及早暴露问题
+   * 在生产环境中，useEffect不会发生这种问题
+   */
+  useEffect(() => {
+    console.log(" mounted");
+    return () => {
+      console.log(" unmounted");
+    };
+  }, []);
   const [questionList, setQuestionList] = useState([
     {
       id: 1,
@@ -55,7 +68,7 @@ export const List: FC = () => {
       //     };
       //   }
       // });
-      const item = draft.find(item => item.id === id);
+      const item = draft.find((item) => item.id === id);
       if (item) {
         item.isPublished = true;
       }
