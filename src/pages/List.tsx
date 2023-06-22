12eb072c62b5c7deba284b765f2d2a7470dfae18
chenhaojie9527 <a18520145493@163.com>
2023-06-22 14:10:50 +0800

    feat: 居中布局

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,43 +1,43 @@
 import React, { FC, useState } from "react";
 import QuestionCard from "../components/QuestionCard";
 
-const listData = [
-  {
-    id: 1,
-    title: "问卷1",
-    isPublished: false,
-    isStarted: false,
-    answerCount: 0,
-    createAt: "3月10日 13:23",
-  },
-  {
-    id: 2,
-    title: "问卷2",
-    isPublished: false,
-    isStarted: false,
-    answerCount: 0,
-    createAt: "3月10日 13:23",
-  },
-  {
-    id: 3,
-    title: "问卷3",
-    isPublished: true,
-    isStarted: false,
-    answerCount: 0,
-    createAt: "3月10日 13:23",
-  },
-  {
-    id: 4,
-    title: "问卷4",
-    isPublished: false,
-    isStarted: false,
-    answerCount: 0,
-    createAt: "3月10日 13:23",
-  },
-];
+// const listData = [
+//   {
+//     id: 1,
+//     title: "问卷1",
+//     isPublished: false,
+//     isStarted: false,
+//     answerCount: 0,
+//     createAt: "3月10日 13:23",
+//   },
+//   {
+//     id: 2,
+//     title: "问卷2",
+//     isPublished: false,
+//     isStarted: false,
+//     answerCount: 0,
+//     createAt: "3月10日 13:23",
+//   },
+//   {
+//     id: 3,
+//     title: "问卷3",
+//     isPublished: true,
+//     isStarted: false,
+//     answerCount: 0,
+//     createAt: "3月10日 13:23",
+//   },
+//   {
+//     id: 4,
+//     title: "问卷4",
+//     isPublished: false,
+//     isStarted: false,
+//     answerCount: 0,
+//     createAt: "3月10日 13:23",
+//   },
+// ];
 
 const List: FC = () => {
-  const [questionList, setQuestionList] = useState(listData);
+  // const [questionList, setQuestionList] = useState(listData);
   return (
     <div className="w-full">
       <div>header</div>
