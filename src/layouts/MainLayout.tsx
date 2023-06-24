5fd55acde0fa4aad5b85e9f34476ff08e8a4670f
chenhaojie9527 <a18520145493@163.com>
2023-06-24 17:16:13 +0800

    feat: 引入outLet模板

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from "react";
+import { Outlet } from "react-router-dom";
 
 const MainLayout: FC = () => {
   return (
     <>
       <div>header</div>
-      <div>main</div>
+      <div>
+        {/* 类似 Vue slot */}
+        <Outlet />
+      </div>
       <div>footer</div>
     </>
   );
