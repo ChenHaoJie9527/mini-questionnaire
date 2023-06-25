63b4081d20e28a50498aeac358fa329a89134a08
chenhaojie9527 <a18520145493@163.com>
2023-06-25 11:42:03 +0800

    feat: 优化404页面

--- src/pages/NotFond.tsx
+++ src/pages/NotFond.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Result } from "antd";
+import { PATHNAME } from "../routers/config";
 
 const NotFound: FC = () => {
-  return <div>404</div>;
+  const nav = useNavigate();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => nav({ pathname: PATHNAME.HOME })}>
+          返回首页
+        </Button>
+      }
+    />
+  );
 };
 
 export default NotFound;
