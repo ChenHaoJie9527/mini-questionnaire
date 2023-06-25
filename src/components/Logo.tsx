7c901d2c8767404a8e9f3573957a1a808d746dc5
chenhaojie9527 <a18520145493@163.com>
2023-06-25 10:07:13 +0800

    feat: logo跳转回首页

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from "react";
 import { FormOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const Logo: FC = () => {
   return (
-    <div className="h-full flex items-center justify-start text-2xl">
-      <FormOutlined className="mr-2 text-3xl" /> <span>小码问卷</span>
-    </div>
+    <Link to={"/"}>
+      <div className="h-full flex items-center justify-start text-2xl cursor-pointer hover:text-[#4178ff]">
+        <FormOutlined className="mr-1 text-2xl" /> <span>小码问卷</span>
+      </div>
+    </Link>
   );
 };
 
