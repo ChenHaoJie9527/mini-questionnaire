b1009a8f37a00cd1c2560332e882d637e7847c05
chenhaojie9527 <a18520145493@163.com>
2023-06-26 19:16:03 +0800

    feat:登录页记住我功能

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { Button, Form, Input, Space, Typography } from "antd";
+import React, { FC, useEffect } from "react";
+import { Button, Checkbox, Form, Input, Space, Typography } from "antd";
 import { homeTheme } from "../themes";
 import { UserAddOutlined } from "@ant-design/icons";
 import { PATHNAME } from "../routers/config";
@@ -10,14 +10,55 @@ const { Title } = Typography;
 interface FormData {
   username: string;
   password: string;
+  remember: boolean;
 }
 
 const Login: FC = () => {
+  const [form] = Form.useForm(); // 获取Form实例对象
   const onFinish = (values: FormData) => {
-    console.log("Success:", values);
+    if (values && values.remember) {
+      setLocal(values);
+    } else {
+      removeLocal();
+    }
   };
+  const setLocal = (values: FormData) => {
+    window.localStorage.setItem(
+      "userinfo",
+      JSON.stringify({
+        USERNAME: values.username,
+        PASSWORD: values.password,
+        REMEMBER: values.remember,
+      })
+    );
+  };
+  const removeLocal = () => {
+    window.localStorage.removeItem("userinfo");
+  };
+  const getLocal = () => {
+    const res = window.localStorage.getItem("userinfo");
+    if (res) {
+      return JSON.parse(res);
+    } else {
+      return {};
+    }
+  };
+  useEffect(() => {
+    const result = getLocal();
+    if (result) {
+      console.log(result);
+      form.setFieldsValue({
+        username: result.PASSWORD,
+        password: result.REMEMBER,
+        remember: result.USERNAME,
+      });
+    }
+  }, []);
   return (
-    <div className="flex items-center justify-center flex-col" style={homeTheme}>
+    <div
+      className="flex items-center justify-center flex-col"
+      style={homeTheme}
+    >
       <Space>
         <Title level={2}>
           <UserAddOutlined />
@@ -26,17 +67,25 @@ const Login: FC = () => {
       </Space>
       <div>
         <Form
+          form={form}
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
           style={{ maxWidth: 600, width: 400 }}
           onFinish={onFinish}
-          initialValues={{ remember: true }}
+          initialValues={{ remember: true }} // 设置初始值
         >
           <Form.Item label="用户名" name="username">
             <Input />
           </Form.Item>
           <Form.Item label="密码" name="password">
             <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>记住我</Checkbox>
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
             <Space>
