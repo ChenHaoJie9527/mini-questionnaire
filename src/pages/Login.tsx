286fd060077fdd76f38c22362d21c65eeb42d62a
chenhaojie9527 <a18520145493@163.com>
2023-06-27 10:19:42 +0800

    feat: 使用antd表单验证添加login登录页

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -74,10 +74,39 @@ const Login: FC = () => {
           onFinish={onFinish}
           initialValues={{ remember: true }} // 设置初始值
         >
-          <Form.Item label="用户名" name="username">
+          <Form.Item
+            label="用户名"
+            name="username"
+            hasFeedback
+            rules={[
+              { required: true, message: "请输入用户名" },
+              {
+                type: "string",
+                min: 5,
+                max: 20,
+                message: "字符长度在 5 - 20 之间",
+              },
+              {
+                pattern: /^\w+$/,
+                message: "只能是字母数字下划线",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码" name="password">
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              { required: true, message: "请输入密码" },
+              {
+                type: "string",
+                min: 6,
+                max: 18,
+                message: "密码长度在 6 - 18 之间",
+              },
+            ]}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item
