e8f761b88ed77e870e37ee1c7b68a0dbb4b6481c
chenhaojie9527 <a18520145493@163.com>
2023-07-05 23:45:05 +0800

    feat: 监听滚动事件

--- src/pages/mange/List.tsx
+++ src/pages/mange/List.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import QuestionCard from "../../components/QuestionCard";
 import Header from "../../components/Header";
 import { useTitle } from "ahooks";
 import { QuestionListType } from "../../utils";
 import { Spin } from "antd";
-import useLoadQuestionDataList from "../../hooks/useLoadQuestionDataList";
 import { ContextCss } from "../../common/styles";
-import ListPagination from "../../components/ListPagination";
 
 const List: FC = () => {
   useTitle("小码问卷 - 问卷列表");
@@ -15,9 +14,32 @@ const List: FC = () => {
     console.log(id);
     console.log(isStarted);
   };
-  const { data: result, loading } = useLoadQuestionDataList();
-  const questionList = result?.data.list ?? [];
-  const total = result?.data.total ?? 0;
+  const [searchParams] = useSearchParams();
+  const [page, setPage] = useState(1);
+  const [questionList, setQuestionList] = useState([]);
+  const [total, setTotal] = useState(0);
+  const haveMoreData = total > questionList.length;
+
+  // 触发加载
+  const tryLoadMore = () => {
+    console.log("滚动加载");
+  };
+
+  // 当页面加载，或者 url 参数 变化时 触发加载
+  useEffect(() => {
+    tryLoadMore();
+  }, [searchParams]);
+
+  // 当页面滚动时，触发加载
+  useEffect(() => {
+    // if (haveMoreData) {
+    //   window.addEventListener("scroll", tryLoadMore);
+    // }
+    window.addEventListener("scroll", tryLoadMore);
+    return () => {
+      window.removeEventListener("scroll", tryLoadMore);
+    };
+  }, [searchParams]);
 
   return (
     <div className="w-11/12 p-5">
@@ -26,19 +48,20 @@ const List: FC = () => {
         style={ContextCss}
         className="scroll-smooth overflow-y-auto scrollbar"
       >
-        {loading && (
+        {/* {loading && (
           <div className="w-full h-full flex items-center justify-center">
             <Spin />
           </div>
-        )}
-        {!loading &&
+        )} */}
+        <div className="h-[3000px]"></div>
+        {
           questionList.length > 0 &&
           questionList.map((item: QuestionListType) => {
             const props = { ...item, onStart };
             return <QuestionCard {...props} key={item.id} />;
           })}
       </div>
-      <ListPagination total={total} current={1} />
+      <div className="text-black">上滑加载更多...</div>
     </div>
   );
 };
